28bd7333b9dfe933b797c123f8bc7b1dd65e3edb
Billy <billygun27@gmail.com>
2018-03-05 22:03:32 +0700

    add mqtt

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mqtt = require('mqtt');
+
+const client  = mqtt.connect('mqtt://iot.eclipse.org');//mqtt://127.0.0.1
+
+
+ 
+client.on('connect', function () {
+  client.subscribe('machine')
+  client.subscribe('sensor')
+  client.publish('sensor', '100')
+ // client.publish('machine','0')
+  //client.publish('machine','1')
+})
+ 
+
+
+var checkmqtt = "F";
+client.on('message', function (topic, message) {
+  // message is Buffer
+ // console.log(message.toString())
+ checkmqtt = topic +"="+message.toString();
+  console.log(checkmqtt);
+
+
+  // client.end()
+})
+
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +52,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+app.get('/mqtt', function(request, response, next) {
+  response.send(checkmqtt);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
